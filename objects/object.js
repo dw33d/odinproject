ea075a8087e0bedd72931afd5f7e0d123859a321
dw33d <170299648+dw33d@users.noreply.github.com>
2024-06-20 22:55:47 +0200

    an object was made

---
+++ objects/object.js
@@ -0,0 +1,9 @@
+// Objects are important
+// basically what dictionaries are in python
+
+let user = new Object();
+ user = {
+    username: "jbourne",
+    age: 23,
+    phone_number:  734566789,
+}
