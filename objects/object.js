3562a2cc02396099cf35d0cb9199bad88042051f
dw33d <170299648+dw33d@users.noreply.github.com>
2024-06-21 15:55:12 +0200

    Finished with Odin Project basics

--- objects/object.js
+++ objects/object.js
@@ -6,4 +6,22 @@ let user = new Object();
     username: "jbourne",
     age: 23,
     phone_number:  734566789,
+    Day_Job: "Accountant",
+    Night_Job: "United States Assassin",
 }
+
+ user.activity = true; // Will only show up in the browser console, not the code editor
+ user.activity = false;
+
+ console.log(user);
+
+console.log(user.username);
+console.log(user.age);
+console.log(user.phone_number);
+
+if (user.activity === true){
+    console.log("Jason Bourne is an active member of the United States Governement, and operating as an agent under the CIA.")
+} else {
+    console.log("Jason Bourne is not a memeber of the US Government, nor is operating under the CIA. Is to be terminated")
+}
+
